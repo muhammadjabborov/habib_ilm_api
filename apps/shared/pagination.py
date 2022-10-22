f3df71da9f2ae85045c06edfd35c899e1928b962
“Muhammad” <zabborovmuhammad291@gmail.com>
2022-10-22 23:29:20 +0500

    initial commit

---
+++ apps/shared/pagination.py
@@ -0,0 +1,9 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+# class CourseCategoryPagination(PageNumberPagination):
+#     page_size = 3
+#     page_size_query_param = 'page_size'
+#     max_page_size = 3
+
+# and add some paginations

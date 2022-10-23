a281a7282ff2e4df50acddbe0e64595c6251efdc
“Muhammad” <zabborovmuhammad291@gmail.com>
2022-10-24 03:16:59 +0500

    initial commit

--- root/settings.py
+++ root/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'habib_education',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Muhammad567809',
-#         'HOST': 'localhost',
-#         'PORT': 5432
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'habib_education',
+        'USER': 'postgres',
+        'PASSWORD': 'Muhammad567809',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -131,14 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "https://habib-ilm-markazi.netlify.app/",
-    "https://habibilmapi.pythonanywhere.com/"
-]
+# CORS_ALLOW_ALL_ORIGINS = True
+#
+# CORS_ORIGIN_WHITELIST = [
+#     "http://localhost:3000",
+#     "http://localhost:3001",
+#     "https://habib-ilm-markazi.netlify.app/",
+#     "https://habibilmapi.pythonanywhere.com/"
+# ]
 
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%Y-%m-%d %H:%M:%S',

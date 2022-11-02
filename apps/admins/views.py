a7dea9cb19e2eba7541cfad671569b1174adae63
“Muhammad” <zabborovmuhammad291@gmail.com>
2022-11-02 13:47:48 +0500

    change some bugs

--- apps/admins/views.py
+++ apps/admins/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 from apps.admins.serializers import RegistrationSerializer
@@ -7,6 +8,7 @@ from apps.admins.serializers import RegistrationSerializer
 
 class RegisterAPIView(GenericAPIView):
     serializer_class = RegistrationSerializer
+    permission_classes = [IsAdminUser]
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
